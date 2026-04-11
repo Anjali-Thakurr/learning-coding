6ca6a348993e00cd1988ac2a48851912c15fbbee
Anjali-Thakurr <a7876110050t@gmail.com>
2026-04-11 21:26:23 +0530

    c++ code aded

---
+++ c++.cpp
@@ -0,0 +1,24 @@
+// finf if number is prime or not 
+#include <iostream> 
+using namespace std; 
+
+int main()
+    {
+      int a ,i;
+      bool prime = true ;
+      cout << "tell if number is prime or not :" ;
+      cin >> a ;
+   for (i=2; i*i<=a;i++){
+    if ( a%i==0){
+     prime= false;
+      break;}
+    }
+  if (prime==true){
+    cout << "prime number";
+  }
+  else {
+    cout << "not prime number ";
+  }
+  
+  return 0;
+  }
